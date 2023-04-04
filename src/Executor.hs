e0c29d5de426c75794e76341b7ba6cfe12ea748e
Koroliuk <koroliuk.yaroslav@gmail.com>
2023-04-04 09:16:09 +0300

    Add update operation

--- src/Executor.hs
+++ src/Executor.hs
@@ -26,6 +26,12 @@ execute (Command "delete" args) context
     | (length args) /= 1 = error "Invalid args"
     | otherwise = deleteByTitle (head args) context
 
+execute (Command "update" (ptype:args)) context = case ptype of
+    "Book"    -> updateBook args context
+    -- "Article" -> updateArticle args context
+    -- "Thesis"  -> updateThesis args context
+    _         -> putStrLn "Invalid publication type"
+
 execute (Command "Help" []) context = 
     putStr("book-store-tool [DBNAME] [-c|--command COMMANDNAME [ARGS]]\n" ++
         "\t[-l|--log LOGNAME]\n" ++
@@ -92,7 +98,68 @@ createThesis [t, conference, city, yearStr, pagesStr, authorsStr] context = do
         savePublication (dbFilePath context) thesis
 createThesis _ content = putStrLn "Invalid arguments for Thesis."
 
+updateBook :: [String] -> Context -> IO ()
+updateBook [t, city, publisher, yearStr, authorsStr] context = do
+    publications <- readAllPublications (dbFilePath context)
+    let alreadySaved = [pub | pub <- publications, title pub == t]
+    if (length alreadySaved > 0)
+        then do 
+            let newContent = [pub | pub <- publications, title pub /= t]
+            let authors = words $ map (\c -> if c == ',' then ' ' else c) authorsStr
+                year = read yearStr :: Int
+                book = Book authors t city publisher year    
+                tempfile = "temp.txt"
+            withFile tempfile WriteMode $ \handle -> do
+                forM_  (newContent ++ [book]) $ \pub -> do
+                    hPutStrLn handle (show pub)
+            removeFile (dbFilePath context)
+            renameFile tempfile (dbFilePath context)
+    else do
+        putStrLn $ "Such book doesnt exist"
+updateBook _ content = putStrLn "Invalid arguments for Book."
 
+updateArticle :: [String] -> Context -> IO ()
+updateArticle [t, journal, yearStr, issueStr, pagesStr, authorsStr] context = do
+    publications <- readAllPublications (dbFilePath context)
+    let alreadySaved = [pub | pub <- publications, title pub == t]
+    if (length alreadySaved > 0)
+        then do
+            let newContent = [pub | pub <- publications, title pub /= t]
+            let authors = words $ map (\c -> if c == ',' then ' ' else c) authorsStr
+                year = read yearStr :: Int
+                issue = read issueStr :: Int
+                (startPage, endPage) = (read $ takeWhile (/= '-') pagesStr, read $ tail $ dropWhile (/= '-') pagesStr) :: (Int, Int)
+                article = Article authors t journal year issue (startPage, endPage)
+                tempfile = "temp.txt"
+            withFile tempfile WriteMode $ \handle -> do
+                forM_  (newContent ++ [article]) $ \pub -> do
+                    hPutStrLn handle (show pub)
+            removeFile (dbFilePath context)
+            renameFile tempfile (dbFilePath context)
+    else do
+        putStrLn $ "Such artickle doesnt exist"
+updateArticle _ content = putStrLn "Invalid arguments for Article."
+
+updateThesis :: [String] -> Context -> IO ()
+updateThesis [t, conference, city, yearStr, pagesStr, authorsStr] context = do
+    publications <- readAllPublications (dbFilePath context)
+    let alreadySaved = [pub | pub <- publications, title pub == t]
+    if (length alreadySaved > 0)
+        then do
+            let newContent = [pub | pub <- publications, title pub /= t]
+            let authors = words $ map (\c -> if c == ',' then ' ' else c) authorsStr
+                year = read yearStr :: Int
+                (startPage, endPage) = (read $ takeWhile (/= '-') pagesStr, read $ tail $ dropWhile (/= '-') pagesStr) :: (Int, Int)
+                thesis = Thesis authors t conference city year (startPage, endPage)
+                tempfile = "temp.txt"
+            withFile tempfile WriteMode $ \handle -> do
+                forM_  (newContent ++ [thesis]) $ \pub -> do
+                    hPutStrLn handle (show pub)
+            removeFile (dbFilePath context)
+            renameFile tempfile (dbFilePath context)
+    else do
+        putStrLn $ "Such thesis doesnt exist"    
+updateThesis _ content = putStrLn "Invalid arguments for Thesis."
 
 savePublication :: FilePath -> Publication -> IO ()
 savePublication path pub = do
